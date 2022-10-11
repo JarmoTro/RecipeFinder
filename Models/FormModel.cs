986cdd1fa3d9a24335e3ebcada6ef80b47272abf
JarmoTro <82982361+JarmoTro@users.noreply.github.com>
2022-10-11 19:01:53 +0300

    add home page

--- Models/FormModel.cs
+++ Models/FormModel.cs
@@ -2,5 +2,14 @@
 {
     public class FormModel
     {
+        public string? Keyword { get; set; }
+
+        public string? MealType { get; set; }
+
+        public string[]? Intolerances { get; set; }
+
+        public int? MaxCookingTime { get; set; }
+
+        public string? Cuisine { get; set; }
     }
 }
